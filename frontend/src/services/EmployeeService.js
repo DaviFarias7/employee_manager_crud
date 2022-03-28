8ffe20cfe641c7daefd0a5af67d56f8af031718e
DaviFarias7 <davic.farias7@gmail.com>
2022-03-27 21:55:11 -0300

    EMPLOYEE_API_BASE_URL = heroku app url

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = process.env.REACT_APP_API_URL;
+const EMPLOYEE_API_BASE_URL = "https://employee-manager-crud.herokuapp.com"; /*process.env.REACT_APP_API_URL;*/ /*"http://localhost:8080/api/v1/employees";*/
 
 class EmployeeService {
 
